997e1e7564bb5386881d70678428bddab87e5efd
William Kong <wilko@umich.edu>
2023-04-07 16:07:44 -0400

    first 2 function done

--- HW7.py
+++ HW7.py
@@ -1,5 +1,5 @@
 
-# Your name:
+# Your name: William Kong
 # Your student id:
 # Your email:
 # List who you have worked with on this project:
@@ -53,7 +53,30 @@ def make_positions_table(data, cur, conn):
 #     created for you -- see make_positions_table above for details.
 
 def make_players_table(data, cur, conn):
-    pass
+    cur.execute("CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY, name TEXT, position_id INTEGER, birthyear INTEGER, nationality TEXT)")
+    squad = data['squad']
+    for player in squad:
+        id = player["id"]
+        name = player["name"]
+        position = player["position"]
+        cur.execute(
+            "SELECT id "
+            "FROM Positions "
+            "WHERE position = ? ",
+            (position,)
+        )
+        position_id = cur.fetchone()[0]
+        birthyear = int(player["dateOfBirth"][0:3])
+        nationality = player["nationality"]
+        cur.execute(
+            "INSERT OR IGNORE INTO Players "
+            "(id, name, position_id, birthyear, nationality) "
+            "VALUES (?,?,?,?,?) ",
+            (id,name,position_id,birthyear,nationality,)
+        )
+
+    conn.commit()
+    
 
 ## [TASK 2]: 10 points
 # Finish the function nationality_search
@@ -66,7 +89,17 @@ def make_players_table(data, cur, conn):
         # the player's name, their position_id, and their nationality.
 
 def nationality_search(countries, cur, conn):
-    pass
+    national_players = []
+    for country in countries:
+        cur.execute(
+                "SELECT name, position_id, nationality "
+                "FROM Players "
+                "WHERE nationality = ? ",
+                (country,)
+            )
+        for row in cur:
+            national_players.append(row)
+    return national_players
 
 ## [TASK 3]: 10 points
 # finish the function birthyear_nationality_search
